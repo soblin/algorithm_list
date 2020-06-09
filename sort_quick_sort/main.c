4b29498d6c371523872905b9d2ce0d02bab1fae4
soblin <hilo.soblin@gmail.com>
2020-06-10 01:32:12 +0900

    [quick_sort]

--- sort_quick_sort/main.c
+++ sort_quick_sort/main.c
@@ -19,15 +19,37 @@ void print(int *data, int size){
   }
 }
 
+int partition(int *data, int start, int end){
+  int pivot = data[end];
+  int pos = start;
+  for(int i = start; i <= end-1; ++i){
+    if(data[i] < pivot){
+      int tmp = data[pos];
+      data[pos] = data[i];
+      data[i] = tmp;
+      pos++;
+    }
+  }
+  int tmp = data[pos];
+  data[pos] = pivot;
+  data[end] = tmp;
+
+  return pos;
+}
+
 void quick_sort(int *data, int start, int end){
-  if(start == end)
-    return;
+  if(start < end){
+    int pivot_pos = partition(data, start, end);
+
+    quick_sort(data, start, pivot_pos-1);
+    quick_sort(data, pivot_pos + 1, end);
+  }
 }
 
 int main(int argc, char **argv){
   // ./a.out 1 2 3 => argc == 4, argv[1] == "1"
   size = (argc) - 1;
   load(argv);
-  simple_insertion(indata, size);
+  quick_sort(indata, 0, size-1);
   print(indata, size);
 }
